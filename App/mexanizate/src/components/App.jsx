2ff7196d1c8694050e4f51bec0b16b753435aefd
ParisRamírezS <pramirezs2705@gmail.com>
2023-09-06 23:06:33 -0600

    Advance pre-compilation test

--- App/mexanizate/src/components/App.jsx
+++ App/mexanizate/src/components/App.jsx
@@ -1,24 +1,14 @@
 import { useState } from 'react'
+import Login from './login/Login.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login />
     </>
   )
 }
 
-export default App
+export { App };
---
+++ App/mexanizate/src/components/utileries/interaction/Hyperlink.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hyperlink() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Hyperlink
